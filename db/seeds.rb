6c1d2948c81fcfdc3ecbeb9e3b7240f1d4d4750e
Ndiema-S <you@example.com>
2022-11-21 17:41:27 +0300

    complete assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+student= Student.create([{ first_name: 'James' }, { last_name: 'Maina' }, {grade:5}])
